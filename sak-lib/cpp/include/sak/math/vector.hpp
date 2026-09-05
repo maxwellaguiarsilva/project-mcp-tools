250ccff4181e2bebc315c3dcc9492370e70cc1ae
Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
2026-09-05 10:44:12 -0300

    refactor: use sak aliases and direct calls in vector adaptors

--- sak-lib/cpp/include/sak/math/vector.hpp
+++ sak-lib/cpp/include/sak/math/vector.hpp
@@ -45,6 +45,10 @@ __using( ::sak::ranges::
 __using( ::sak::ranges::views::
 	,rotated
 )
+__using( ::std::ranges::
+	,range_adaptor_closure
+	,viewable_range
+)
 
 
 //	euclidean norm of a vector: sqrt( dot( vector, vector ) )
@@ -58,9 +62,9 @@ inline constexpr auto length = __length{ };
 
 //	unit vector in the same direction, lazy when the input is a view
 //	direct-application closure: the pipe applies to the whole vector, no element-wise transform
-struct __normalize : ::std::ranges::range_adaptor_closure< __normalize >
+struct __normalize : range_adaptor_closure< __normalize >
 {
-	template< ::std::ranges::viewable_range t_vector >
+	template< viewable_range t_vector >
 	constexpr auto operator ( ) ( t_vector&& vector ) const noexcept
 	{
 		return	::std::forward< t_vector >( vector ) / length( vector );
@@ -74,11 +78,11 @@ inline constexpr auto normalize = __normalize{ };
 struct __cross
 {
 	template< typename t_right >
-	struct closure : ::std::ranges::range_adaptor_closure< closure< t_right > >
+	struct closure : range_adaptor_closure< closure< t_right > >
 	{
 		t_right m_right;
 		constexpr explicit closure( t_right right ) : m_right( right ) { }
-		template< ::std::ranges::viewable_range t_left >
+		template< viewable_range t_left >
 		constexpr auto operator ( ) ( t_left&& left ) const noexcept
 		{
 			return	__cross{ }( ::std::forward< t_left >( left ), m_right );
@@ -88,7 +92,7 @@ struct __cross
 	template< is_crossable t_left, is_crossable t_right >
 	constexpr auto operator ( ) ( const t_left& left, const t_right& right ) const noexcept
 	{
-		return	( left | rotated( 1 ) ) * ( right | rotated( 2 ) ) - ( left | rotated( 2 ) ) * ( right | rotated( 1 ) );
+		return	rotated( left, 1 ) * rotated( right, 2 ) - rotated( left, 2 ) * rotated( right, 1 );
 	}
 
 	template< typename t_right >
@@ -103,12 +107,12 @@ inline constexpr auto cross = __cross{ };
 struct __rotate
 {
 	template< typename t_axis >
-	struct closure : ::std::ranges::range_adaptor_closure< closure< t_axis > >
+	struct closure : range_adaptor_closure< closure< t_axis > >
 	{
 		t_axis m_axis;
 		float m_angle;
 		constexpr closure( t_axis axis, float angle ) : m_axis( axis ), m_angle( angle ) { }
-		template< ::std::ranges::viewable_range t_vector >
+		template< viewable_range t_vector >
 		constexpr auto operator ( ) ( t_vector&& vector ) const noexcept
 		{
 			return	__rotate{ }( ::std::forward< t_vector >( vector ), m_axis, m_angle );
