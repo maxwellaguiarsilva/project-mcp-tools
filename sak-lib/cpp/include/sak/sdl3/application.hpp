90de41032ce21e38824739479e5eaa4ed9836aa1
Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
2026-09-05 14:14:27 -0300

    feat: add sdl3 application skeleton class

---
+++ sak-lib/cpp/include/sak/sdl3/application.hpp
@@ -0,0 +1,59 @@
+//	
+//	Copyright (C) 2026 Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
+//	
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//	
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//	
+//	You should have received a copy of the GNU General Public License
+//	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//	
+//	
+//	File:   include/sak/sdl3/application.hpp
+//	Author: Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
+//	
+//	Created on 2026-09-05 11:59:08
+//	
+
+
+
+#pragma once
+#ifndef header_guard_498437518
+#define header_guard_498437518
+
+#include <sak/sak.hpp>
+#include <type_traits>
+#include <sak/ensure.hpp>
+#include <sak/pattern/bitmask.hpp>
+#include <SDL3/SDL.h>
+
+
+
+namespace sak {
+namespace sdl3 {
+
+
+
+
+
+
+class application
+{
+public:
+	//	application( ) = default;
+
+};
+
+
+} } 
+
+
+#endif
+
+
