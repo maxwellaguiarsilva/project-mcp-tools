ee226cd6829d3d8e116e5cb540caa008b436cc3e
Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
2026-09-05 14:14:24 -0300

    feat: add pattern bitmask skeleton class

---
+++ sak-lib/cpp/include/sak/pattern/bitmask.hpp
@@ -0,0 +1,57 @@
+//	
+//	Copyright (C) 2026 Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
+//	
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//	
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//	
+//	You should have received a copy of the GNU General Public License
+//	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//	
+//	
+//	File:   include/sak/pattern/bitmask.hpp
+//	Author: Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
+//	
+//	Created on 2026-09-05 11:59:08
+//	
+
+
+
+#pragma once
+#ifndef header_guard_119678595
+#define header_guard_119678595
+
+#include <sak/sak.hpp>
+#include <type_traits>
+#include <concepts>
+
+
+
+namespace sak {
+namespace pattern {
+
+
+
+
+
+
+class bitmask
+{
+public:
+	//	bitmask( ) = default;
+
+};
+
+
+} } 
+
+
+#endif
+
+
