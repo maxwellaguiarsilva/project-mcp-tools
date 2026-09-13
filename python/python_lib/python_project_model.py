1b384aa82fdd437cb87c3c19495e7e1c7359a925
Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
2026-09-13 15:06:10 -0300

    style: replace license headers with SPDX identifiers

--- python/python_lib/python_project_model.py
+++ python/python_lib/python_project_model.py
@@ -1,26 +1,8 @@
 #!/usr/bin/python3
 
 #   
-#   Copyright (C) 2026 Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
-#   
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
-#   
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-#   
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#   
-#   
-#   File:   python/python_lib/python_project_model.py
-#   Author: Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
-#   
-#   Created on 2026-02-01 23:45:00
+#   SPDX-FileCopyrightText: 2026 Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
+#   SPDX-License-Identifier: GPL-3.0-or-later
 #   
 
 
